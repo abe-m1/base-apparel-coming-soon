ceafcbf9d0a860bf2da05fddf532a025a96e2ad9
abe menashy <abestbet1@gmail.com>
2020-09-23 15:29:37 -0700

    remove logs

--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@ const form = document.querySelector('#form');
 
 function validate(e) {
   e.preventDefault();
-  console.log('validate');
-  // const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const regEx = /^.+@\w+\.\w+$/;
   console.log(input.value);
   if (regEx.test(String(input.value).toLowerCase())) {
@@ -16,7 +14,6 @@ function validate(e) {
     form.classList.remove('form-error');
     errorMessage.style.display = 'none';
   } else {
-    console.log('not valid', form);
     errorCircle.style.display = 'block';
     errorMessage.style.display = 'block';
     form.classList.add('form-error');
